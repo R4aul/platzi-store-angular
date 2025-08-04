adf6ff23ec28d7c3282d1eb4ccae56d8687bb03c
Raul Damian Rafael <raul.damian220@gmail.com>
2025-08-04 00:03:24 -0600

    feat:implementation of interceptors to measure time

---
+++ src/app/interceptors/time.interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { tap } from "rxjs/operators";
+
+export const timeInterceptor: HttpInterceptorFn = (req, next) => {
+  const start = performance.now();
+  return next(req)
+  .pipe(
+    tap(()=>{
+      const time = (performance.now() - start) + 'ms'
+      console.log(req.url, time );
+      
+    })
+  );
+};
